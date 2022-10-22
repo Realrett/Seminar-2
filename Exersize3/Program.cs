fa00898305a0205fb52492ed4023ef7358d0c08e
Realrett <gosha-student@mail.ru>
2022-10-22 19:49:17 +0700

    Третья задача

---
+++ Exersize3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите порядковый номер дня недели");
+string dayNumber = Console.ReadLine();
+int result = int.Parse(dayNumber);
+if(result >= 1 && result <= 5)
+    Console.WriteLine("Рабочий день");
+if(result > 5 && result <= 7)
+    Console.WriteLine("Выходной день");
+if(result > 7 || result < 1)
+Console.WriteLine("Научитесь ПРАВИЛЬНО считать дни недели!!!");
