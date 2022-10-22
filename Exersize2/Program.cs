932773657747f7417797445d672a87c54c63383f
Realrett <gosha-student@mail.ru>
2022-10-22 19:29:19 +0700

    Вторая задача

---
+++ Exersize2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число ");
+string numberA = Console.ReadLine();
+int number = Math.Abs(int.Parse(numberA));
+int result = number / 100;
+if(result > 0)
+    Console.WriteLine($"Третьей цифрой числа {number} является {result % 10}");
+else
+    Console.WriteLine($"Третья цифра в числе отсутсвует");
